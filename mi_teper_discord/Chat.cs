e3a6f320010ecf8374b5161233819877bb059656
AA <Student@CAB4-2>
2024-10-16 16:32:53 +1000

    готово!

--- mi_teper_discord/Chat.cs
+++ mi_teper_discord/Chat.cs
@@ -10,4 +10,9 @@ public partial class Chat
     public string Text { get; set; } = null!;
 
     public string Author { get; set; } = null!;
+
+    public override string ToString()
+    {
+        return $"Пользователь {Author} отправил/а: {Text}";
+    }
 }
--- mi_teper_discord/MainWindow.xaml.cs
+++ mi_teper_discord/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.ComponentModel;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Text;
 using System.Text.Json;
 using System.Windows;
@@ -17,28 +20,41 @@ namespace mi_teper_discord
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
         public Discord1135Context context;
 
         HttpClient httpClient = new HttpClient();
         private DispatcherTimer timer = null;
+ 
 
-        public Chat Chat { get; set; }
-        public List<Chat> Chats { get; set; } = new List<Chat>();
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public Chat Chat { get; set; } = new();
+        private List<Chat> chats;
+        public List<Chat> Chats 
+        { 
+            get=> chats;
+            set 
+            {
+                chats = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Chats)));
+            } }
 
         public MainWindow()
         {
             InitializeComponent();
             httpClient.BaseAddress = new Uri("http://localhost:5132/api/");
             DataContext = this;
+            timerStart();
+            
         }
 
         public void timerStart()
         {
             timer = new DispatcherTimer();
             timer.Tick += new EventHandler(timerTick);
-            timer.Interval = new TimeSpan(0, 1, 0);
+            timer.Interval = new TimeSpan(0, 0, 5);
             timer.Start();
         }
 
@@ -51,7 +67,28 @@ namespace mi_teper_discord
         public async void GetMessage()
         {
 
-            var responce = await httpClient.PostAsync($"Messages/GetMessages", context);          
+            var responce = await httpClient.PostAsync($"Messages/GetMessages", new StringContent("", Encoding.UTF8, "application/json"));          
+            if (responce.StatusCode != System.Net.HttpStatusCode.OK)
+            {
+                var result = await responce.Content.ReadAsStringAsync();
+                MessageBox.Show("все плохо!");
+                return;
+            }
+            else
+            {
+                var messages = await responce.Content.ReadFromJsonAsync<List<Chat>>();
+                Chats = new List<Chat>(messages);
+                
+
+            }
+        }
+
+        private async void SendMessage(object sender, RoutedEventArgs e)
+        {
+            string arg = JsonSerializer.Serialize(Chat);
+            var responce = await httpClient.PostAsync($"Messages/SendMessages",
+                new StringContent(arg, Encoding.UTF8, "application/json"));
+
             if (responce.StatusCode != System.Net.HttpStatusCode.OK)
             {
                 var result = await responce.Content.ReadAsStringAsync();
@@ -59,15 +96,18 @@ namespace mi_teper_discord
             }
             else
             {
-                var answer = await responce.Content.ReadFromJsonAsync<Search>();
-                MessageBox.Show($"Нашелся человек по паспорту - {answer.ToString()}");
-                Close();
+                MessageBox.Show("Сообщение отправлено!");
+                
             }
+
         }
 
-        private void SendMessage(object sender, RoutedEventArgs e)
+        private void EnterIsPressed(object sender, KeyEventArgs e)
         {
-
+            if (e.Key == Key.Enter)
+            {
+                SendMessage(null, null);
+            }
         }
     }
 }
